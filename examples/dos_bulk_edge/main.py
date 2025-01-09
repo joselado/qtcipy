90b6ed1cf5198dae2e35c2b08c17bf85b03d1443
joselado <jose.lado@aalto.fi>
2025-01-09 10:10:43 +0200

    Update

---
+++ examples/dos_bulk_edge/main.py
@@ -0,0 +1,32 @@
+import os ; import sys
+sys.path.append(os.getcwd()+"/../../src")
+
+from qtcipy.tbscftk import hamiltonians
+import numpy as np
+
+L = 8 # exponential length
+H = hamiltonians.chain(L) # get the Hamiltonian
+
+import time
+ii = 2**(L-1) # bulk
+jj = 0 # edge
+
+# perform the SCF
+SCF = H.get_SCF_Hubbard(U=3.0)
+SCF.solve(use_qtci=False,use_kpm = False)
+
+
+import matplotlib.pyplot as plt
+
+(es,ds) = SCF.get_dos(i=[ii],kpm_scale=4,delta=1e-1)
+plt.plot(es,ds,label="Bulk")
+(es,ds) = SCF.get_dos(i=[jj],kpm_scale=4,delta=1e-1)
+plt.plot(es,ds,label="Edge")
+
+plt.legend()
+
+plt.xlabel("Energy")
+plt.ylabel("DOS")
+
+plt.show()
+
